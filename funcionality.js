30634c0faa33753dc8326bc8af99f461f3001f55
amjordan <alvaromartinezjordan@yahoo.com>
2021-02-13 07:46:14 +0100

    Funcionalidad con ionContent

--- funcionality.js
+++ funcionality.js
@@ -4,69 +4,86 @@
 // UNA VEZ SE LIMPIA EL CLEAR INTERVAL SE JODIO. YA HAY QUE REFRESCAR TODO
 // GASTA MUCHA MEMORIA TENER UN INTERVAL CORRIENDO TODO EL RATO
 //SOLO COMPARA NUMEROS  //AQUI SE PODRIA METER UN LISTENER DE ALGUN MODO
+//lo que me detiene la app cuando falla es el alert por lo que sin el no va bien del todo
+
+
 const d = document;
 let $first = d.querySelector(`#first_input`),
     $second = d.querySelector(`#second_input`),
     $third = d.querySelector(`#third_input`),
     $checkText = d.querySelector(`#checkText`);
+    var $checking;
+    
+    const createVerification = () => {
+    const ionCard = d.createElement('ion-card'),
+    ionCardContent = d.createElement('ion-card-content')
+    ionCardContent.textContent = $checking + ``;
+    ionCard.appendChild(ionCardContent);
+    $checkText.appendChild(ionCard);
+}
+
+
 
 function verifyContent() {
     setFirstFocus()
-    function setFirstFocus() {
-        setTimeout(() => {
-            this.first_input.setFocus();
-        }, 200)
-    }
-    function setSecondFocus() {
-        setTimeout(() => {
-            this.second_input.setFocus();
-        }, 100)
-    }
-    function setThirdFocus() {
-        setTimeout(() => {
-            this.third_input.setFocus();
-        }, 100)
-    }
+
+    function setFirstFocus() {setTimeout(() => {this.first_input.setFocus();}, 200)};
+    function setSecondFocus() {setTimeout(() => {this.second_input.setFocus();}, 100)};
+    function setThirdFocus() {setTimeout(() => {this.third_input.setFocus();}, 100)};
+
 
     let firstInputInterval = setInterval(() => {
         if ($first.value > 1) {
             checkFirstInput();
         }
     }, 1000);
+
     let secondInputInterval = setInterval(() => {
         if ($first.value > 1 && $first.value == $second.value) {
             checkSecondInput();
         } if ($second.value > 1 && $first.value != $second.value) {
-            alert("Los codigos insertados no son iguales");//...
+            $checking = `Los codigos insertados no son iguales`;
+            createVerification();
             clearInterval(secondInputInterval);
+            //alert("Los codigos insertados no son iguales");//...
         }
     }, 1000);
+
     let thirdInputInterval = setInterval(() => {
         if ($second.value > 1 && $second.value == $third.value) {
             checkThirdInput();
         } if ($third.value > 1 && $second.value != $third.value) {
-            alert("Los codigos insertados no son iguales");//...
+            $checking = `Los codigos insertados no son iguales`;
+            createVerification();
             clearInterval(thirdInputInterval);
+            //alert("Los codigos insertados no son iguales");//...
         }
     }, 1000);
+
+
+
+
 
     function checkFirstInput() {
         clearInterval(firstInputInterval);
         setSecondFocus();
+        $checking = "Primer valor añadido";
+        createVerification();
         console.log("Primer valor añadido");
     }
     function checkSecondInput() {
         clearInterval(secondInputInterval);
         setThirdFocus();
+        $checking = "Segundo valor añadido";
+        createVerification();
         console.log("Segundo valor añadido");
     }
     function checkThirdInput() {
         clearInterval(thirdInputInterval);
-        console.log("Los codigos añadidos coinciden")
-        //$checkText.innerHTML = `<p>Los codigos coinciden</p>`
+        $checking = "Los codigos introducidos coinciden";
+        createVerification();
+        console.log("Los codigos introducidos coinciden");
     }
 }
 verifyContent()
-
-//numero prueba
-//123321321321545
+//465465465465465
