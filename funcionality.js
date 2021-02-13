fc6f795a7a7ad87e844c76d0515597e6954be294
amjordan <alvaromartinezjordan@yahoo.com>
2021-02-13 07:00:42 +0100

    Funcionality integrated :)

--- funcionality.js
+++ funcionality.js
@@ -4,58 +4,69 @@
 // UNA VEZ SE LIMPIA EL CLEAR INTERVAL SE JODIO. YA HAY QUE REFRESCAR TODO
 // GASTA MUCHA MEMORIA TENER UN INTERVAL CORRIENDO TODO EL RATO
 //SOLO COMPARA NUMEROS  //AQUI SE PODRIA METER UN LISTENER DE ALGUN MODO
-
-
 const d = document;
-let $first = d.getElementById("first_input"),
-    $second = d.getElementById("second_input"),
-    $third = d.getElementById("third_input"),
-    $checkText = d.getElementById("checkText");
+let $first = d.querySelector(`#first_input`),
+    $second = d.querySelector(`#second_input`),
+    $third = d.querySelector(`#third_input`),
+    $checkText = d.querySelector(`#checkText`);
 
 function verifyContent() {
-    d.getElementById("first_input").focus();
+    setFirstFocus()
+    function setFirstFocus() {
+        setTimeout(() => {
+            this.first_input.setFocus();
+        }, 200)
+    }
+    function setSecondFocus() {
+        setTimeout(() => {
+            this.second_input.setFocus();
+        }, 100)
+    }
+    function setThirdFocus() {
+        setTimeout(() => {
+            this.third_input.setFocus();
+        }, 100)
+    }
 
-    //FIRST INPUT INTERVAL
     let firstInputInterval = setInterval(() => {
         if ($first.value > 1) {
             checkFirstInput();
         }
-    }, 2000);
-
-    //SECOND INPUT INTERVAL
+    }, 1000);
     let secondInputInterval = setInterval(() => {
         if ($first.value > 1 && $first.value == $second.value) {
             checkSecondInput();
         } if ($second.value > 1 && $first.value != $second.value) {
-            alert("Los codigos insertados no son iguales");
+            alert("Los codigos insertados no son iguales");//...
             clearInterval(secondInputInterval);
         }
     }, 1000);
-
-    //THIRD INPUT INTERVAL
     let thirdInputInterval = setInterval(() => {
         if ($second.value > 1 && $second.value == $third.value) {
             checkThirdInput();
-        } if /**/($third.value > 1 && $second.value != $third.value) {
-            alert("Los codigos insertados no son iguales");
+        } if ($third.value > 1 && $second.value != $third.value) {
+            alert("Los codigos insertados no son iguales");//...
             clearInterval(thirdInputInterval);
         }
     }, 1000);
 
     function checkFirstInput() {
         clearInterval(firstInputInterval);
-        d.getElementById("second_input").focus();
+        setSecondFocus();
         console.log("Primer valor añadido");
     }
     function checkSecondInput() {
         clearInterval(secondInputInterval);
-        d.getElementById("third_input").focus();
+        setThirdFocus();
         console.log("Segundo valor añadido");
     }
     function checkThirdInput() {
         clearInterval(thirdInputInterval);
         console.log("Los codigos añadidos coinciden")
-        $checkText.innerHTML = `<p>Los codigos coinciden</p>`
+        //$checkText.innerHTML = `<p>Los codigos coinciden</p>`
     }
 }
 verifyContent()
+
+//numero prueba
+//123321321321545
