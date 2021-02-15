1a4389f20bdfae2a02a4da1ab3e9f7b143924c38
amjordan <alvaromartinezjordan@yahoo.com>
2021-02-15 05:15:41 +0100

    Added funcionality for stopped first set interval at 2 hours

--- funcionality.js
+++ funcionality.js
@@ -1,27 +1,23 @@
 // ERRORES A SOLUCIONAR:
-// SOLO COMPARA NUMEROS     // $first.value > "1" en vez de  > 1
 // NO DA TIEMPO A INGRESAR EL PRIMER VALOR, DEBERIA HABER MAS DELAY EN EL SETINTERVAL O QUE COJA UN EVENTO PORQUE SI ESTÁ OYENDO Y LE QUEDA MENOS DE UN SEGUNDO APENAS ENTRAN CARACTERES
 // UNA VEZ SE LIMPIA EL CLEAR INTERVAL SE JODIO. YA HAY QUE REFRESCAR TODO
-// GASTA MUCHA MEMORIA TENER UN INTERVAL CORRIENDO TODO EL RATO
-//SOLO COMPARA NUMEROS  //AQUI SE PODRIA METER UN LISTENER DE ALGUN MODO
-//lo que me detiene la app cuando falla es el alert por lo que sin el no va bien del todo
 //añadir funcionalidad para meter codigos a mano sin interval
 //quedaria darle color a los ion content
 //que genere un check verification nada mas en vez de tres?
-//en vez de setinterval estaria bien saber cual es la longitud de los codigos de barras
-//al darle al ok del alert que refreque la pagina
+//al darle al ok del alert que refresque la pagina
 //Se podria quitar el checking de error ya que hay un alert ya
+//Borrar los console.log
 
 const d = document;
 let $first = d.querySelector(`#first_input`),
     $second = d.querySelector(`#second_input`),
     $third = d.querySelector(`#third_input`),
     $checkText = d.querySelector(`#checkText`);
-    var $checking;
-    
-    const createVerification = () => {
+let $checking, sumInterval = 0;
+
+const createVerification = () => {
     const ionCard = d.createElement('ion-card'),
-    ionCardContent = d.createElement('ion-card-content')
+        ionCardContent = d.createElement('ion-card-content');
     ionCardContent.textContent = $checking + ``;
     ionCard.appendChild(ionCardContent);
     $checkText.appendChild(ionCard);
@@ -29,29 +25,43 @@ let $first = d.querySelector(`#first_input`),
 
 const presentAlert = () => {
     const alert = d.createElement('ion-alert');
-    alert.header = 'Datos invalidos';
-    alert.subHeader = 'Por favor verifica los datos';
-    alert.message = 'Incorrección en los datos';
+    alert.header = 'Los datos no coinciden';
+    //alert.subHeader = 'Por favor verifique los datos';
+    alert.message = 'Por favor verifique los datos';
+    //alert.message = 'Incorrección en los datos';
     alert.buttons = ['Ok'];
+    d.body.appendChild(alert);
+    return alert.present();
+}
 
+const inactivityAlert = () => {
+    const alert = d.createElement('ion-alert');
+    alert.header = 'Aplicación detenida';
+    //alert.subHeader = 'La aplicación se detiene tras dos horas de inactividad';
+    alert.message = 'La aplicación se detiene tras dos horas de inactividad. Por favor, refresque para comenzar de nuevo.'; //PONER EN EL SEGUNDO IF DE FIRSTINPUTINTERVAL 7200 PARA QUE SEAN DOS HORAS
+    alert.buttons = ['Ok'];
     d.body.appendChild(alert);
     return alert.present();
 }
 
 
 
-function verifyContent() {
+function verifyContent() {                          /***********FUNCION PRINCIPAL***********/
     setFirstFocus()
 
-    function setFirstFocus() {setTimeout(() => {this.first_input.setFocus();}, 200)};
-    function setSecondFocus() {setTimeout(() => {this.second_input.setFocus();}, 100)};
-    function setThirdFocus() {setTimeout(() => {this.third_input.setFocus();}, 100)};
+    function setFirstFocus() { setTimeout(() => { this.first_input.setFocus(); }, 200) };
+    function setSecondFocus() { setTimeout(() => { this.second_input.setFocus(); }, 100) };
+    function setThirdFocus() { setTimeout(() => { this.third_input.setFocus(); }, 100) };
 
 
     let firstInputInterval = setInterval(() => {
         if ($first.value > '1') {
             checkFirstInput();
+        } if (sumInterval == 5/*7200 son dos horas*/) {//...
+            inactivityAlert();
+            clearInterval(firstInputInterval);
         }
+        sumInterval++;
     }, 1000);
 
     let secondInputInterval = setInterval(() => {
@@ -85,22 +95,17 @@ function verifyContent() {
         setSecondFocus();
         $checking = "Primer valor añadido";
         createVerification();
-        console.log("Primer valor añadido");
     }
     function checkSecondInput() {
         clearInterval(secondInputInterval);
         setThirdFocus();
         $checking = "Segundo valor añadido";
         createVerification();
-        console.log("Segundo valor añadido");
     }
     function checkThirdInput() {
         clearInterval(thirdInputInterval);
         $checking = "Los codigos introducidos coinciden";
         createVerification();
-        console.log("Los codigos introducidos coinciden");
     }
 }
 verifyContent()
-//465465465465465
-//sdfsadfsadfsadfsadf
